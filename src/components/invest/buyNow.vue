f69a29e225e5a42257882240a91b005878f3c859
zj <zh>
2018-04-16 09:01:49 +0800

    投资页面

---
+++ src/components/invest/buyNow.vue
@@ -0,0 +1,135 @@
+<template>
+<div>
+    <Header :headerTitle="headerTitle" :routerLink="routerLink"></Header>
+    <div class="buyDetailsBox">
+        <div class="itemTitle">
+           <span></span>{{headerTitle}}
+        </div>
+        <ul class="buyDetailsList">
+            <li>
+                <span>年化收益</span>
+                <span class="redColor">12.00%</span>
+            </li>
+            <li>
+                <span>项目期限</span>
+                <span class="mainColor1">7天</span>
+            </li>
+            <li>
+                <span>剩余额度(元）</span>
+                <span class="mainColor1">1500.00</span>
+            </li>
+        </ul>
+        <p class="inverstDdesc" >剩余额度(元)</p>
+        <input type="text" class="inverstMoney mainColor1" placeholder="100元的整数倍">
+        <ul class="buyDetailsList">
+            <li>
+                <span>预期收益(元）</span>
+                <span >2300</span>
+            </li>
+            <li>
+                <span>投资金额(元）</span>
+                <span >1200</span>
+            </li>
+            <li>
+                <span>可用余额(元）</span>
+                <span >1500.00</span>
+            </li>
+        </ul>
+        <router-link to="/bidSuccess"  class="sureBuyBtn  LiearGradientMain2">
+            确认购买
+        </router-link>
+        <div class="readProtocol mainColor1">
+            <label><input type="checkbox" name="" value="">我已阅读</label>
+            <a href="JavaScript:;" class="mainColor1">《投资服务协议》</a>与
+            <a href="JavaScript" class="mainColor1">《借款协议》</a>
+        </div>
+    </div>
+
+
+</div>
+
+</template>
+<script >
+import Header from "@/components/common/header"
+export default{
+    components:{
+        Header
+    },
+    data(){
+        return {
+           headerTitle:"项目20180123",
+           routerLink:"/investDetails",
+        }
+    }
+
+}
+</script>
+<style scoped lang="scss">
+@function p($px) {
+    @return $px/75px*1rem;
+}
+.buyDetailsBox{
+    background:#fff;
+    margin-top:p(20px);
+    .itemTitle{
+        height: p(100px);
+        font-size: p(36px);
+        color: #333;
+        font-weight: normal;
+        border-bottom: p(2px) dashed #e5e5e5;
+        padding-top: p(40px);
+        padding-left:p(65px);
+    }
+    .buyDetailsList{
+        padding-left: p(56px);
+        padding-right: p(45px);
+        font-size:p(28px);
+        li{
+            padding-top:p(40px);
+            overflow:hidden;
+            color:#999;
+            span:first-child{
+                float:left;
+            }
+            span:last-child{
+                float:right;
+            }
+        }
+    }
+    .inverstDdesc{
+        text-align: center;
+        padding-top:p(78px);
+        font-size:p(32px);
+        color: #6c6c6c;
+    }
+    .inverstMoney{
+        font-size: p(42px);
+        color: #999;
+        display: block;
+        margin: 0 auto;
+        border: none;
+        border-bottom: p(3px) solid #e5e5e5;
+        width: p(326px);
+        margin-top:p(45px);
+        padding: p(5px) 0;
+        color: #ffbb20;
+        text-align: center;
+    }
+    .sureBuyBtn{
+        width: p(534px);
+        height: p(94px);
+        border-radius:p(50px);
+        display: block;
+        margin: p(40px) auto;
+        text-align:center;
+        line-height:p(94px);
+        font-size:p(30px);
+        color:#fff;
+    }
+    .readProtocol{
+        text-align:center;
+    }
+}
+
+
+</style>
---
+++ src/components/invest/investDetails.vue
@@ -0,0 +1,128 @@
+<template>
+    <div>
+       <Header  :headerTitle="headerTitle" :routerLink="routerLink"></Header>
+        <div class="investDetailsBox">
+            <h3>
+                项目标题  2018234
+            </h3>
+            <div class="progress">
+                40%
+            </div>
+            <table>
+                <tbody>
+                    <tr class="tableNumber">
+                        <td>10万</td>
+                        <td>90天</td>
+                        <td>9.19万</td>
+                    </tr>
+                    <tr class="tableTitle">
+                        <td>项目总额</td>
+                        <td>期限</td>
+                        <td>剩余金额</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="bidDetails">
+            <ul>
+                <li><span>起投金额：</span><span>100元起</span></li>
+                <li><span>还款方式：</span><span>一次性还本付息</span></li>
+                <li><span>剩余时间：</span><span>14天23时12分</span></li>
+                <li><span>计息时间：</span><span>满标当日计息</span></li>
+            </ul>
+        </div>
+
+        <router-link to="/buyNow" class="RightBugBtn" >立即购买</router-link>
+    </div>
+</template>
+<script >
+import Header from '@/components/common/header'
+export default{
+    components:{
+        Header
+    },
+    data() {
+       return {
+          //头部链接
+          routerLink:"/investIndex",
+          //头部内容
+          headerTitle:"购买详情"
+       }
+    }
+
+}
+</script>
+<style scoped lang='scss'>
+@function p($px){
+    @return $px/75px*1rem;
+}
+.investDetailsBox{
+    background:#fff;
+    margin-top:p(10px);
+    h3{
+        padding:p(10px) p(20px);
+        font-size:p(34px);
+        color:#535353;
+    }
+    .progress{
+        width:p(250px);
+        height:p(250px);
+        border:p(16px) solid #ffbb20;
+        border-radius:50%;
+        margin:p(20px) auto;
+        text-align:center;
+        font-size:p(70px);
+        line-height:p(250px);
+        color:#535353;
+    }
+    table{
+        width: 100%;
+        td{
+            width:33.33%;
+            text-align:center;
+            padding:p(10px) 0px;
+        }
+        .tableNumber{
+            font-size:p(28px);
+            color:#535353;
+        }
+        .tableTitle{
+            color: rgba(153, 153, 153, 0.99);
+            font-size:p(22px);
+        }
+    }
+}
+
+.bidDetails{
+    ul{
+        padding-left:p(20px);
+        background:#fff;
+        padding-top:p(20px);
+        padding-bottom:p(20px);
+        margin:p(20px) auto;
+        li{
+            padding:p(16px) 0px;
+            font-size:p(28px);
+            span:first-child{
+                color:#999;
+            }
+             span:last-child{
+                color:#444;
+            }
+        }
+    }
+}
+.RightBugBtn{
+    height:p(100px);
+    width:100%;
+    display:block;
+    position:fixed;
+    bottom:0px;
+    left:0px;
+    background:#ffbb20;
+    text-align:center;
+    font-size:p(32px);
+    color:#fff;
+    line-height:p(100px);
+}
+</style>
